31724520030ada8947c9c9e0604e268104d214c7
jiawenquan <1173551915@qq.com>
2019-03-22 01:14:51 +0800

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component'; // 添加到组件
 import {FormsModule} from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-import {MessagesComponent} from './messages/messages.component'; // <-- NgModel lives here
+import {MessagesComponent} from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 @NgModule({
   // 声明的组件集
@@ -13,12 +15,14 @@ import {MessagesComponent} from './messages/messages.component'; // <-- NgModel 
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   // 导入的驱动模块
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
